050243c6098962b905cbca516bf0f94282ea2ddd
Samuel Santos Barreto Pereira <samucateclas@gmail.com>
2021-03-29 22:23:31 -0300

    First part Finished

---
+++ AbilityScoreTester/AbilityScoreTester/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AbilityScoreTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            while (true)
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+                calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Calculated ability score: " + calculator.Score);
+                Console.WriteLine("Press Q to quit, any other key to continue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+            }
+        }
+        /// <summary>
+        /// Writes a prompt and reads an int value
+        /// </summary>
+        /// <param name="lastUsedValue"></param>
+        /// <param name="prompt"></param>
+        /// <returns>The int value read, or the default value if unable to parse</returns>
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine(" using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+        /// <summary>
+        /// Writes a prompt and reads an double value
+        /// </summary>
+        /// <param name="lastUsedValue"></param>
+        /// <param name="prompt"></param>
+        /// <returns>The double value read, or the default value if unable to parse</returns>
+        static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+            if (double.TryParse(line, out double value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine(" using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
