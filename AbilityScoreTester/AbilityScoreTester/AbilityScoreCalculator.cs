1054a885f3130e387f5d3de64a6b5d59781ff0be
Samuel Santos Barreto Pereira <samucateclas@gmail.com>
2021-03-30 23:05:32 -0300

    Program Done

--- AbilityScoreTester/AbilityScoreTester/AbilityScoreCalculator.cs
+++ AbilityScoreTester/AbilityScoreTester/AbilityScoreCalculator.cs
@@ -21,7 +21,7 @@ namespace AbilityScoreTester
             double divided = RollResult / DivideBy;
 
             // Add AddAmount to the result of that division
-            int added = AddAmount += (int)divided;
+            int added = AddAmount + (int)divided;
 
             // if result is too small, use Minimum
             if (added < Minimum)
